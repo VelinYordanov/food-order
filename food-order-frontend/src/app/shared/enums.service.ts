303d36af37d395004aeec276fe1fb6c60d69a544
Velin Yordanov <velin.iordanov@gmail.com>
2020-08-29 18:23:27 +0300

    Angular enum service.

---
+++ food-order-frontend/src/app/shared/enums.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EnumData } from './models/enum-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnumsService {
+  private readonly BASE_URL: string = "/api/enums";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCities() {
+    return this.httpClient.get<EnumData>(`${this.BASE_URL}/cities`);
+  }
+
+  getAddressTypes() {
+    return this.httpClient.get<EnumData>(`${this.BASE_URL}/address-types`);
+  }
+}
