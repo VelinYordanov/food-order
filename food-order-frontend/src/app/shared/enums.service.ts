ae922544aa73e7aa2c17d3234d6a8f4100ff73f3
Velin Yordanov <velin.iordanov@gmail.com>
2020-08-29 18:30:04 +0300

    Enum service client side caching.

--- food-order-frontend/src/app/shared/enums.service.ts
+++ food-order-frontend/src/app/shared/enums.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 import { EnumData } from './models/enum-data';
 
 @Injectable({
@@ -8,13 +10,30 @@ import { EnumData } from './models/enum-data';
 export class EnumsService {
   private readonly BASE_URL: string = "/api/enums";
 
+  private cities$: Observable<EnumData[]>;
+  private addressTypes$: Observable<EnumData[]>;
+
   constructor(private httpClient: HttpClient) { }
 
   getCities() {
-    return this.httpClient.get<EnumData>(`${this.BASE_URL}/cities`);
+    if (!this.cities$) {
+      this.cities$ = this.httpClient.get<EnumData[]>(`${this.BASE_URL}/cities`)
+        .pipe(
+          shareReplay()
+        );
+    }
+
+    return this.cities$;
   }
 
   getAddressTypes() {
-    return this.httpClient.get<EnumData>(`${this.BASE_URL}/address-types`);
+    if (!this.addressTypes$) {
+      this.addressTypes$ = this.httpClient.get<EnumData[]>(`${this.BASE_URL}/address-types`)
+        .pipe(
+          shareReplay()
+        );
+    }
+
+    return this.addressTypes$;
   }
 }
