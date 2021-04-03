3e5fd3f081e3f918f63e1a8a58fb6f1278168f11
VelinYordanov <velin.iordanov@gmail.com>
2021-04-03 21:41:15 +0300

    Added nginx dev container, changes in create database script and docker compose.

--- food-order-db/create-database.sql
+++ food-order-db/create-database.sql
@@ -2,6 +2,10 @@ CREATE LOGIN FoodOrder WITH PASSWORD = '$(FOOD_ORDER_PASSWORD)';
 EXEC sp_addsrvrolemember @loginame= 'FoodOrder', @rolename = 'dbcreator';
 GO
 EXECUTE AS LOGIN = 'FoodOrder'; 
-CREATE DATABASE FoodOrder;
-ALTER DATABASE FoodOrder SET AUTO_CLOSE OFF;
+CREATE DATABASE FoodOrderDb;
+ALTER DATABASE FoodOrderDb SET AUTO_CLOSE OFF;
+GO
+USE FoodOrderDb
+CREATE USER FoodOrder for LOGIN FoodOrder
+ALTER ROLE db_owner ADD MEMBER FoodOrder
 GO
