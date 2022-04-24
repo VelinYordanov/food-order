bac132d3d84a4fb9e62ac7e4fbac18dfdcf09aef
Velin Yordanov <velin.iordanov@gmail.com>
2022-04-25 01:11:12 +0300

    Removed caching from enum service.

--- food-order-frontend/src/app/customers/services/enums.service.ts
+++ food-order-frontend/src/app/customers/services/enums.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
 import { EnumData } from '../models/enum-data';
 
 @Injectable({
@@ -10,42 +8,17 @@ import { EnumData } from '../models/enum-data';
 export class EnumsService {
   private readonly BASE_URL: string = "/api/enums";
 
-  private cities$: Observable<EnumData[]>;
-  private addressTypes$: Observable<EnumData[]>;
-  private orderStatuses$: Observable<EnumData[]>;
-
   constructor(private httpClient: HttpClient) { }
 
   getCities() {
-    if (!this.cities$) {
-      this.cities$ = this.httpClient.get<EnumData[]>(`${this.BASE_URL}/cities`)
-        .pipe(
-          shareReplay()
-        );
-    }
-
-    return this.cities$;
+    return this.httpClient.get<EnumData[]>(`${this.BASE_URL}/cities`)
   }
 
   getAddressTypes() {
-    if (!this.addressTypes$) {
-      this.addressTypes$ = this.httpClient.get<EnumData[]>(`${this.BASE_URL}/address-types`)
-        .pipe(
-          shareReplay()
-        );
-    }
-
-    return this.addressTypes$;
+    return this.httpClient.get<EnumData[]>(`${this.BASE_URL}/address-types`)
   }
 
   getOrderStatuses() {
-    if (!this.orderStatuses$) {
-      this.orderStatuses$ = this.httpClient.get<EnumData[]>(`${this.BASE_URL}/order-statuses`)
-        .pipe(
-          shareReplay()
-        );
-    }
-
-    return this.orderStatuses$;
+    return this.httpClient.get<EnumData[]>(`${this.BASE_URL}/order-statuses`)
   }
 }
