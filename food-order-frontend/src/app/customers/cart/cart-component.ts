1cdc0effa4110dfe05b357ea622fed532726680a
Velin Yordanov <velin.iordanov@gmail.com>
2022-04-30 15:38:46 +0300

    Changed some things in cart component.

--- food-order-frontend/src/app/customers/cart/cart-component.ts
+++ food-order-frontend/src/app/customers/cart/cart-component.ts
@@ -1,47 +1,34 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subject } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable, Subject } from 'rxjs';
 import { first, map, takeUntil } from 'rxjs/operators';
 import { AuthenticationService } from 'src/app/shared/services/authentication.service';
 import { CartService } from 'src/app/shared/services/cart.service';
+import { cartItemsSumSelector } from '../store/cart/cart.selectors';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart-component.html',
   styleUrls: ['./cart-component.scss'],
 })
-export class CartComponent implements OnInit, OnDestroy {
-  numberOfFoods: number;
-
-  private onDestroy$ = new Subject<void>();
+export class CartComponent implements OnInit {
+  numberOfFoods$: Observable<number>;
 
   constructor(
     private router: Router, 
-    private cartService: CartService,
+    private store: Store,
     private authenticationService : AuthenticationService) {}
 
   ngOnInit(): void {
-    this.cartService.selectedItems$
-      .pipe(
-        takeUntil(this.onDestroy$),
-        map((items) =>
-          items
-            .map((item) => item.quantity)
-            .reduce((total, current) => total + current, 0)
-        )
-      )
-      .subscribe((numberOfFoods) => (this.numberOfFoods = numberOfFoods));
+    this.numberOfFoods$ = this.store.select(cartItemsSumSelector);
   }
 
   goToAddress() {
+    // TODO: use store here
     this.authenticationService.user$
     .pipe(
       first(),
     ).subscribe(user => user ? this.router.navigate(['customer', 'order', 'address']) : this.router.navigate(['login']));
-  }
-
-  ngOnDestroy(): void {
-    this.onDestroy$.next();
-    this.onDestroy$.complete();
   }
 }
