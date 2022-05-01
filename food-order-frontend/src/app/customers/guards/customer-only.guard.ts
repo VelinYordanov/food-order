b6439929c3089e2c2d39eafe7d2d211c4b493f19
Velin Yordanov <velin.iordanov@gmail.com>
2022-05-01 13:15:53 +0300

    Customer only guard uses the store.

--- food-order-frontend/src/app/customers/guards/customer-only.guard.ts
+++ food-order-frontend/src/app/customers/guards/customer-only.guard.ts
@@ -1,28 +1,30 @@
 import { Injectable } from '@angular/core';
-import {  UrlTree, Router, CanLoad, Route, UrlSegment } from '@angular/router';
+import { UrlTree, Router, CanLoad, Route, UrlSegment } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { first, map } from 'rxjs/operators';
 import { AuthenticationService } from 'src/app/shared/services/authentication.service';
+import { loggedInUserSelector } from 'src/app/shared/store/authentication/authentication.selectors';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerOnlyGuard implements CanLoad {
   constructor(
-    private authenticationService: AuthenticationService,
-    private router: Router) {}
+    private store: Store,
+    private router: Router) { }
 
   canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    return this.authenticationService.user$
+    return this.store.select(loggedInUserSelector)
       .pipe(
         first(),
         map(user => {
-          if(!user) {
+          if (!user) {
             return this.router.parseUrl('');
           }
-  
+
           return user.authorities?.includes('ROLE_CUSTOMER');
         })
       )
-  }  
+  }
 }
